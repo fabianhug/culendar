02862cf2ed8343ca6336bd97ecec4edc6c39ce64
alexand3rwilke <alexander-wilke@hotmail.de>
2024-05-25 23:43:06 +0200

    update ownable

--- src/Registry.sol
+++ src/Registry.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.20;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { Ownable } from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 /**
  * @title Culendar shielded address registry
