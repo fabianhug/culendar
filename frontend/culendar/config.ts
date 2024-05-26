99ab70687908b226e875b2262e2698b5685ea9cb
alexand3rwilke <alexander-wilke@hotmail.de>
2024-05-26 11:54:01 +0200

    change abi and stuff

---
+++ frontend/culendar/config.ts
@@ -0,0 +1,11 @@
+import { http, createConfig } from '@wagmi/core'
+import { mainnet, sepolia } from '@wagmi/core/chains'
+
+export const config = createConfig({
+  chains: [sepolia],
+  transports: {
+    // [mainnet.id]: http(),
+    [sepolia.id]: http('https://gateway.tenderly.co/public/sepolia'),
+  },
+})
+
